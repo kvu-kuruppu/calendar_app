dc7b79dbc966d6c59a26412cd0c8d46bac129f31
Your Name <Your Email>
2022-09-02 13:48:28 +0530

    SfCalendar ontap date

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,7 +2,6 @@ import 'package:calendar_app/consts/routes.dart';
 import 'package:calendar_app/controller/task_controller.dart';
 import 'package:calendar_app/models/task.dart';
 import 'package:calendar_app/services/notification_service.dart';
-import 'package:calendar_app/utils/show_error_dialog.dart';
 import 'package:calendar_app/widgets/button.dart';
 import 'package:calendar_app/widgets/task_tile.dart';
 import 'package:date_picker_timeline/date_picker_timeline.dart';
@@ -11,6 +10,7 @@ import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:intl/intl.dart';
 import 'dart:developer' as devtools show log;
 import 'package:get/get.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -106,28 +106,44 @@ class _HomePageState extends State<HomePage> {
                 const SizedBox(
                   height: 20,
                 ),
-                DatePicker(
-                  // Calendar
-                  DateTime.now(),
-                  height: 100,
-                  width: 80,
-                  initialSelectedDate: DateTime.now(),
-                  selectionColor: const Color.fromARGB(255, 12, 103, 179),
-                  selectedTextColor: Colors.white,
-                  dateTextStyle: const TextStyle(
-                    fontSize: 35,
-                    color: Colors.grey,
+                // Calendar
+                SfCalendar(
+                  view: CalendarView.month,
+                  firstDayOfWeek: 1,
+                  selectionDecoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.red,
+                      width: 2,
+                    ),
                   ),
-                  dayTextStyle: const TextStyle(
-                    fontSize: 15,
-                    color: Colors.grey,
-                  ),
-                  monthTextStyle: const TextStyle(
-                    fontSize: 15,
-                    color: Colors.grey,
-                  ),
-                  onDateChange: (date) {},
+                  onTap: calendarTapped,
                 ),
+                // DatePicker(
+                //   DateTime.now(),
+                //   height: 100,
+                //   width: 80,
+                //   initialSelectedDate: DateTime.now(),
+                //   selectionColor: const Color.fromARGB(255, 12, 103, 179),
+                //   selectedTextColor: Colors.white,
+                //   dateTextStyle: const TextStyle(
+                //     fontSize: 35,
+                //     color: Colors.grey,
+                //   ),
+                //   dayTextStyle: const TextStyle(
+                //     fontSize: 15,
+                //     color: Colors.grey,
+                //   ),
+                //   monthTextStyle: const TextStyle(
+                //     fontSize: 15,
+                //     color: Colors.grey,
+                //   ),
+                //   onDateChange: (date) {
+                //     setState(() {
+                //       _selectDate = date;
+                //     });
+                //   },
+                // ),
                 const SizedBox(
                   height: 20,
                 ),
@@ -140,6 +156,14 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  void calendarTapped(CalendarTapDetails details) {
+    details.targetElement == CalendarElement.calendarCell;
+    setState(() {
+      _selectDate = details.date!;
+    });
+    print(DateFormat.yMd().format(details.date!));
+  }
+
   _showTasks() {
     return Expanded(
       child: Obx(
@@ -147,26 +171,52 @@ class _HomePageState extends State<HomePage> {
           return ListView.builder(
             itemCount: _taskController.taskList.length,
             itemBuilder: (_, index) {
-              // devtools.log(_taskController.taskList.length.toString());
-              return AnimationConfiguration.staggeredList(
-                position: index,
-                child: SlideAnimation(
-                  child: FadeInAnimation(
-                    child: Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            _showSimpleDialog(index);
-                          },
-                          child: TaskTile(
-                            _taskController.taskList[index],
+              Task task = _taskController.taskList[index];
+              if (task.repeat == 'Daily') {
+                return AnimationConfiguration.staggeredList(
+                  position: index,
+                  child: SlideAnimation(
+                    child: FadeInAnimation(
+                      child: Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              _showSimpleDialog(index);
+                            },
+                            child: TaskTile(
+                              _taskController.taskList[index],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
+                );
+              }
+              if (task.date == DateFormat.yMd().format(_selectDate)) {
+                return AnimationConfiguration.staggeredList(
+                  position: index,
+                  child: SlideAnimation(
+                    child: FadeInAnimation(
+                      child: Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              _showSimpleDialog(index);
+                            },
+                            child: TaskTile(
+                              _taskController.taskList[index],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              } else {
+                return Container();
+              }
+              // devtools.log(_taskController.taskList.length.toString());
             },
           );
         },
